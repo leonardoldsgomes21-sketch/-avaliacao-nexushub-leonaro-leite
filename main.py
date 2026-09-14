d3af96be7b1fe9917108dc1212a004efeab5e894
leonardoldsgomes21-sketch <leonardo.lds.gomes21@gmail.com>
2026-09-14 12:26:42 +0000

    lista de dicionario e bancada

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+#Etapa 1: Cadastro da Startup e Projetos (Dicionários e Listas)
+startup = {
+	"nome": "CyberPulse Tech",
+	"segmento": "Segurança da Informação",
+	"ano_adesao": "2026",
+}
+
+solucoes_ativas = ["Firewall IA", "Scan de Vulnerabilidades"]
+
+print("Nome da startup:", startup["nome"])
+print("Segmento:", startup["segmento"])
+print("Primeiro produto:", solucoes_ativas[0])
+
+#Etapa 2: Mapeamento das Bancadas de Trabalho (Matriz 2D)
+bancadas = [
+	[1, 0],
+	[0, 1],
+]
+
+print("Bancada N1:", bancadas[0][0])
+print("Bancada N2:", bancadas[0][1])
+print("Bancada S1:", bancadas[1][0])
+print("Bancada S2:", bancadas[1][1])
+print("Legenda: 1 = Ocupado e 0 = Livre")
 
